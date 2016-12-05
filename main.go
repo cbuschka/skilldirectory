e452c4977430d0533e2aa4e9337d84bc1088637c
Brian Hoehne <brian.hoehne@maryville.com>
2016-12-05 09:26:10 -0600

    closses #6

--- main.go
+++ main.go
@@ -5,14 +5,7 @@ import (
 	"skilldir/router"
 )
 
-// var skillsConnector = data.NewAccessor(data.NewFileWriter("skills/"))
-
 func main() {
-	// addSkillsToView()
 	router := router.StartRouter()
 	http.ListenAndServe(":8080", router)
 }
-
-// func addSkillsToView() {
-// 	skillsConnector.Save("Golang", model.NewSkill("Golang", "language"))
-// }
