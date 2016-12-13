80afb8e6c42a76701968048619f2f96242ae5ff5
Hopding <andrew.dillon.j@gmail.com>
2016-12-13 15:49:16 -0600

    Simplified error handling in handler/handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -19,39 +19,29 @@ func MakeHandler(fn func(http.ResponseWriter, *http.Request, controller.RESTCont
 func Handler(w http.ResponseWriter, r *http.Request, cont controller.RESTController) {
 	log.Printf("Handling Skills Request: %s", r.Method)
 	cont.Base().Init(w, r, data.NewAccessor(data.NewFileWriter("skills/")))
-	var err error
-	var statusCode int
 
+	var err error
 	switch r.Method {
 	case http.MethodGet:
 		err = cont.Get()
-		statusCode = http.StatusNotFound
-		switch err.(type) {
-		case *errors.NoSuchIDError:
-			statusCode = http.StatusNotFound
-		}
 	case http.MethodPost:
 		err = cont.Post()
-		statusCode = http.StatusBadRequest
+	case http.MethodDelete:
+		err = cont.Delete()
+	}
+
+	var statusCode int
+	if err != nil {
 		switch err.(type) {
-		case *errors.MarshalingError:
-			statusCode = http.StatusBadRequest
-		case *errors.InvalidSkillTypeError:
+		case *errors.MarshalingError, *errors.InvalidSkillTypeError, *errors.MissingSkillIDError:
 			statusCode = http.StatusBadRequest
 		case *errors.SavingError:
 			statusCode = http.StatusInternalServerError
-		}
-	case http.MethodDelete:
-		err = cont.Delete()
-		switch err.(type) {
 		case *errors.NoSuchIDError:
 			statusCode = http.StatusNotFound
-		case *errors.MissingSkillIDError:
-			statusCode = http.StatusBadRequest
+		default:
+			statusCode = http.StatusInternalServerError
 		}
-	}
-
-	if err != nil {
 		log.Printf("SkillsHandler Method: %s, Err: %v", r.Method, err)
 		http.Error(w, err.Error(), statusCode)
 	}
