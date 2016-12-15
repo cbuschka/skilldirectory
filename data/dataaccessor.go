f42b32b8209eb4343b7954cc3485c2b60da415bd
Hopding <andrew.dillon.j@gmail.com>
2016-12-15 13:20:25 -0600

    Fixed variable naming error

--- data/dataaccessor.go
+++ data/dataaccessor.go
@@ -2,7 +2,7 @@ package data
 
 /*
 DataAccessor is a wrapper type for implementations of the DataAccess interface.
- */
+*/
 type DataAccessor struct {
 	DataAccess
 }
@@ -10,7 +10,7 @@ type DataAccessor struct {
 /*
 NewAccessor() is a constructor for the DataAccessor type. It accepts implementations of the
 DataAccess interface, and returns an instance of DataAccessor wrapping the passed-in DataAccess implementation.
- */
+*/
 func NewAccessor(access DataAccess) DataAccessor {
 	return DataAccessor{access}
 }
@@ -19,7 +19,7 @@ func NewAccessor(access DataAccess) DataAccessor {
 DataAccess represents an interface for accessing and managing a data storage system (e.g. a filesystem or database).
 Implementations of DataAccess should ensure that the objects passed to them are properly serialized under, and can be
 retrieved by providing, the specified key string.
- */
+*/
 type DataAccess interface {
 	Save(key string, object interface{}) error
 	Read(key string, object interface{}) error
@@ -30,7 +30,7 @@ type DataAccess interface {
 
 /*
 ReadAllInterface implementations must contain a GetType() function that returns the type of the implementor.
- */
+*/
 type ReadAllInterface interface {
 	GetType() interface{}
 }
