36709b96e29112b1f7eb627adf5aae8de8eb8104
Hopding <andrew.dillon.j@gmail.com>
2016-12-16 09:03:06 -0600

    Added tests for new TeamMembersController

--- model/teammember_test.go
+++ model/teammember_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestNewTeamMember(t *testing.T) {
 	tmOne := NewTeamMember("1234", "Yogi Bear", "Smarter Than Average Bear")
 	tmTwo := TeamMember{
-		ID: "1234",
-		Name: "Yogi Bear",
+		ID:    "1234",
+		Name:  "Yogi Bear",
 		Title: "Smarter Than Average Bear",
 	}
 	if tmOne != tmTwo {
