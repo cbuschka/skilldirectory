dfc39fbd736d65773d301fbb25dddd2b13dfc093
Hopding <andrew.dillon.j@gmail.com>
2016-12-19 15:22:08 -0600

    Refactored checkForId() logic for improved readability

--- controller/basecontroller.go
+++ controller/basecontroller.go
@@ -24,9 +24,31 @@ func (bc *BaseController) Init(w http.ResponseWriter, r *http.Request, session d
 // will be returned. If not, then an empty string is returned ("").
 func checkForId(url *url.URL) string {
 	base := path.Base(url.RequestURI())
-	if url.EscapedPath() != "/skills" && url.EscapedPath() != "/skills/" &&
-		url.EscapedPath() != "/teammembers" && url.EscapedPath() != "/teammembers/" {
-		return base
+	if isValidEndpoint(url.EscapedPath()) {
+		return ""
 	}
-	return ""
+	return base
+}
+
+// isValidEndpoint() returns true if endpoint is an endpoint being
+// served by the SkillDirectory server AND doesn't contain an ID.
+func isValidEndpoint(endpoint string) bool {
+	endpoints := []string {
+		"/skills", "/skills/",
+		"/teammembers", "/teammembers/",
+	}
+	if stringSliceContains(endpoints, endpoint) {
+		return true
+	}
+	return false
+}
+
+// stringSliceContains() returns true if slice contains target, false if not.
+func stringSliceContains(slice []string, target string) bool {
+	for _, element := range slice {
+		if element == target {
+			return true
+		}
+	}
+	return false
 }
