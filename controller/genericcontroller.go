5b1da4bb1b73f80d3282963c76c74e914afda29a
Hopding <andrew.dillon.j@gmail.com>
2016-12-22 12:37:55 -0600

    Restored genericcontroller.go

--- controller/genericcontroller.go
+++ controller/genericcontroller.go
@@ -8,7 +8,7 @@ func (c GenericController) Base() *BaseController {
 	return c.BaseController
 }
 
-func (c GenericController) Get() error                                                { return nil }
-func (c GenericController) Post() error                                               { return nil }
-func (c GenericController) Delete(table, id string, primary_key_cols ...string) error { return nil }
-func (c GenericController) Put() error                                                { return nil }
+func (c GenericController) Get() error    { return nil }
+func (c GenericController) Post() error   { return nil }
+func (c GenericController) Delete() error { return nil }
+func (c GenericController) Put() error    { return nil }
