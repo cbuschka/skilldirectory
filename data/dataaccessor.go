b2a100f3cd3355c85787ee872c40eeb7a00af71d
Hopding <andrew.dillon.j@gmail.com>
2017-01-03 12:40:45 -0600

    Removed genericcontroller.go; Removed DataAccessor type

--- data/dataaccessor.go
+++ data/dataaccessor.go
@@ -1,35 +1,23 @@
 package data
 
 /*
-DataAccessor is a wrapper type for implementations of the DataAccess interface.
-*/
-type DataAccessor struct {
-	DataAccess
-}
-
-/*
-NewAccessor() is a constructor for the DataAccessor type. It accepts implementations of the
-DataAccess interface, and returns an instance of DataAccessor wrapping the passed-in DataAccess implementation.
-*/
-func NewAccessor(access DataAccess) DataAccessor {
-	return DataAccessor{access}
-}
-
-/*
-DataAccess represents an interface for accessing and managing a data storage system (e.g. a database).
-Implementations of DataAccess should ensure that the objects passed to them are properly serialized under, and can be
-retrieved by providing, the specified key string.
+DataAccess represents an interface for accessing and managing a data storage
+system (e.g. a database). Implementations of DataAccess should ensure that the
+objects passed to them are properly serialized under, and can be retrieved by
+providing, the specified key string.
 */
 type DataAccess interface {
 	Save(table, key string, object interface{}) error
 	Read(table, key string, object interface{}) error
 	ReadAll(table string, readType ReadAllInterface) ([]interface{}, error)
-	FilteredReadAll(table string, opts CassandraQueryOptions, readType ReadAllInterface) ([]interface{}, error)
+	FilteredReadAll(table string, opts CassandraQueryOptions,
+		readType ReadAllInterface) ([]interface{}, error)
 	Delete(table, id string, primary_key_cols ...string) error
 }
 
 /*
-ReadAllInterface implementations must contain a GetType() function that returns the type of the implementor.
+ReadAllInterface implementations must contain a GetType() function that returns
+the type of the implementor.
 */
 type ReadAllInterface interface {
 	GetType() interface{}
