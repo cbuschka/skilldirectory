0f5f2f1dcfbe8a7ce8fbab0d62865d9ccb507661
Hopding <andrew.dillon.j@gmail.com>
2017-01-05 07:58:57 -0600

    Fixed method name in tmskill tests

--- model/tmskill_test.go
+++ model/tmskill_test.go
@@ -21,7 +21,7 @@ func TestNewTMSkillDefaults(t *testing.T) {
 	}
 	//Verify that all of tmSkillOne and tmSkillTwo's fields are equal
 	if reflect.DeepEqual(tmSkillDTOOne, tmSkillDTOTwo) {
-		t.Error("constructor newTMSkillDefaults() produced incorrect TMSkill.")
+		t.Error("constructor NewTMSkillDefaults() produced incorrect TMSkill.")
 	}
 }
 
@@ -41,20 +41,20 @@ func TestNewTMSkillSetDefaults(t *testing.T) {
 	}
 	// Verify that all of tmSkillOneDTO and tmSkillTwoDTO's fields are equal.
 	if tmSkillDTOOne == tmSkillDTOTwo {
-		t.Error("constructor newTMSkillSetDefaults() produced incorrect TMSkill.")
+		t.Error("constructor NewTMSkillSetDefaults() produced incorrect TMSkill.")
 	}
 
 	tmSkill := NewTMSkillSetDefaults("TMSkillID", "SkillID", "TeamMemberID", true, 3)
 	// Verify that the constructor clips proficiencies > 5 to 5
 	tmSkill = NewTMSkillSetDefaults("TMSkillID", "SkillID", "TeamMemberID", true, 9000)
 	if tmSkill.Proficiency != 5 {
-		t.Error("constructor newTMSkillSetDefaults() failed to cap proficiency > 5")
+		t.Error("constructor NewTMSkillSetDefaults() failed to cap proficiency > 5")
 	}
 
 	// Verify that the constructor clips proficiencies < 0 to 0
 	tmSkill = NewTMSkillSetDefaults("TMSkillID", "SkillID", "TeamMemberID", true, -9000)
 	if tmSkill.Proficiency != 0 {
-		t.Error("constructor newTMSkillSetDefaults() failed to cap proficiency < 0")
+		t.Error("constructor NewTMSkillSetDefaults() failed to cap proficiency < 0")
 	}
 }
 
@@ -63,12 +63,12 @@ func TestTMSkill_SetProficiency(t *testing.T) {
 
 	tmSkill.SetProficiency(9000)
 	if tmSkill.Proficiency != 5 {
-		t.Error("method TMSkill.setProficiency() failed to cap proficiency > 5")
+		t.Error("method TMSkill.SetProficiency() failed to cap proficiency > 5")
 	}
 
 	tmSkill.SetProficiency(-9000)
 	if tmSkill.Proficiency != 0 {
-		t.Error("method TMSkill.setProficiency() failed to cap proficiency < 0")
+		t.Error("method TMSkill.SetProficiency() failed to cap proficiency < 0")
 	}
 }
 
