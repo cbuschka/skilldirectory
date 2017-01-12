264f95ea41de0b315289b3b5e5deea65e9b2fd84
Dane Johnson <dane_johnson@ymail.com>
2017-01-12 10:48:29 -0600

    Added header to allow axios delete requests

--- handler/handler.go
+++ handler/handler.go
@@ -17,6 +17,7 @@ func MakeHandler(fn func(http.ResponseWriter, *http.Request, controller.RESTCont
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		fn(w, r, cont, session)
 	}
 }
