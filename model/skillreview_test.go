06e2635a2717d6c06b5b404b380044c4b6853e82
Dane Johnson <dane_johnson@ymail.com>
2017-02-15 14:14:30 -0600

    Updated skillreviews and tests

--- model/skillreview_test.go
+++ model/skillreview_test.go
@@ -6,28 +6,19 @@ import (
 )
 
 func TestNewSkillReview(t *testing.T) {
-	srDTOOne := NewSkillReview("1234", "2345", "3456", "blahblahblah", "1234",
-		true).NewSkillReviewDTO("Coding", "Javatar")
-	srDTOTwo := SkillReviewDTO{
-		SkillReview: SkillReview{
-			ID:           "1234",
-			SkillID:      "2345",
-			TeamMemberID: "3456",
+	srOne := NewSkillReview("blahblahblah", true)
+	srTwo := SkillReview {
 			Body:         "blahblahblah",
-			Timestamp:    "1234",
 			Positive:     true,
-		},
-		SkillName:      "Coding",
-		TeamMemberName: "Javatar",
 	}
 	// Verify that all of srOne and srTwo's fields are equal
-	if !reflect.DeepEqual(srDTOOne, srDTOTwo) {
+	if !reflect.DeepEqual(srOne, srTwo) {
 		t.Errorf("constructor newSkillReview() produced incorrect SkillReview.")
 	}
 }
 
 func TestGetSkillReviewType(t *testing.T) {
-	s := NewSkillReview("", "", "", "", "", true)
+	s := NewSkillReview("", true)
 	if !reflect.DeepEqual(s.GetType(), SkillReview{}) {
 		t.Error("SkillReview getType not returning empty skill review")
 	}
