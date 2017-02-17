37846e67d27e077136d1147bdbdfb21d30ee036e
JMH <jheld135@gmail.com>
2017-02-16 22:06:58 -0600

    Added tests for POST on users endpoint

--- controller/userscontroller_test.go
+++ controller/userscontroller_test.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"skilldirectory/data"
+	"skilldirectory/model"
 	"testing"
 
 	"github.com/Sirupsen/logrus"
@@ -50,13 +52,45 @@ func TestPut(t *testing.T) {
 }
 
 func TestOptions(t *testing.T) {
-	request := httptest.NewRequest(http.MethodOptions, "/users", bytes.NewBufferString(""))
+	request := httptest.NewRequest(http.MethodOptions, "/api/users", bytes.NewBufferString(""))
 	sc := getUsersController(request, &data.MockDataAccessor{})
 
 	err := sc.Options()
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestPostUser_NoCode(t *testing.T) {
+	body := getReaderForNewCredentials("", "")
+	request := httptest.NewRequest(http.MethodPost, "/api/users", body)
+	uc := getUsersController(request, &data.MockDataAccessor{})
+
+	err := uc.Post()
+	if err == nil {
+		t.Errorf("Expected error: %s", err.Error())
+	}
+}
+
+func TestPostUser_NoClientID(t *testing.T) {
+	body := getReaderForNewCredentials("foobarbaz", "")
+	request := httptest.NewRequest(http.MethodPost, "/api/users", body)
+	uc := getUsersController(request, &data.MockDataAccessor{})
+
+	err := uc.Post()
+	if err == nil {
+		t.Errorf("Expected error: %s", err.Error())
+	}
+}
+
+/*
+getReaderForNewUser is a helper function for a new Skill with the given id, name, and skillType.
+This Skill is then marshaled into JSON. A new Reader is created and returned for the resulting []byte.
+*/
+func getReaderForNewCredentials(code, clientID string) *bytes.Reader {
+	newCredentials := model.AuthCredentials{Code: code, Id: clientID}
+	b, _ := json.Marshal(newCredentials)
+	return bytes.NewReader(b)
 }
 
 /*
